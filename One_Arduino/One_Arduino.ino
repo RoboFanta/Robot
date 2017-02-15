9f5d3e0cd57b0eac4591ff79a280ef1b8fde17bf
blueburningcoder <f.karg10@gmail.com>
2017-02-15 15:20:39 +0100

    Sync

--- One_Arduino/One_Arduino.ino
+++ One_Arduino/One_Arduino.ino
@@ -120,10 +120,10 @@ typedef void (*void_ptr)();
 #define lmain_length 5
 #define lback_length 3
 
-#define SPEED_VSLOW   25
-#define SPEED_SLOW    50
-#define SPEED_MIDDLE  100
-#define SPEED_FAST    150
+#define SPEED_VSLOW   30
+#define SPEED_SLOW    65 
+#define SPEED_MIDDLE  120
+#define SPEED_FAST    160
 #define SPEED_VFAST   200
 
 #define DIRECTION_RIGHT_3 127
@@ -142,7 +142,7 @@ typedef void (*void_ptr)();
 #define MOTOR_MAX 255
 
 // Servo winkel
-#define SERVO_OPEN 148
+#define SERVO_OPEN 143
 #define SERVO_CLOSE 43
 
 //Scan heufigkeit 1/scan_freq
@@ -180,7 +180,7 @@ void_ptr current_action = states[0];
 
 // for ultrasonic sensor
 int distance, variance, old_distance = 25, diff;
-NewPing sonar(TRIGPIN, ECHOPIN, 20);
+NewPing sonar(TRIGPIN, ECHOPIN, 25);
 // 20 cm of maximum distance -> up to 1200 us. We might get as far as ~5mm in that time.
 
 
@@ -220,7 +220,50 @@ bool readSecond = false;
 bool isBall = false;
 bool wasBall = false;
 int iteration = 0;
-bool did_init = false;
+int turnstate = 0;
+
+
+
+// from paul
+
+int maxspeed = 150; //Wert von 0 bis 255
+int softturn = 40; //Wert von 0 bis 255
+int hardturn = 40;   //Wert von 25 bis 255
+float hardturnfaktor = 3;
+
+int turnspeed = 150;
+
+int approxspd = 150;
+int approxsoftturn = 100; //
+
+int ruecksetzspeed = 150;
+int ruecksetzdelay = 2000; //Wie lange soll er nach dem Grabben zurückfahren (Speed = approxspd1)
+int drehzyklus = 200; //Optimierung des Drehzyklus ab dem Momnt, in dem alle hinteren Sensoren "0" lesen
+
+int molipl = 6; //ANALOG WRITE
+int molimi = 10; //ANALOG WRITE
+int morepl = 5; //ANALOG WRITE
+int moremi = 9; //ANALOG WRITE             //MOTOREN
+
+
+int vogali = 3;
+int voli = 4;
+int vomi = 14;
+int vore = 7;
+int vogare = 8;
+int vogalival = 0;
+int volival = 0;
+int vomival = 0;
+int voreval = 0;
+int vogareval = 0;                              //VORDERE SENSORREIHE
+
+bool hardturnlinks = false;
+bool turnlinks = false;
+
+float slow_factor = 1.0;
+
+
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -256,6 +299,9 @@ void setup() {          // initializing happens here
 
   pinMode(LED_BUILTIN, OUTPUT);
 
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
+
 } // end of setup
 
 
@@ -280,7 +326,7 @@ void loop() {         // Main Control loop
 
   current_action(); // TODO: use @Victors method.
 
-  set_motors();
+  // set_motors();
 
   ++iteration;
 
@@ -296,9 +342,10 @@ void read_sensors() {         // Reading the ultrasonic- and light sensors
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Ultrasonic reading happens here
-  if (iteration % 10 == 0 && askForDist) {
-    variance = distance;
-    old_distance = distance && variance ? distance : old_distance;    // latest accurate distance value
+  if (iteration % 20 == 0 && askForDist) {
+    // variance = distance;
+    old_distance = distance ? distance : old_distance;    // latest accurate distance value
+    // old_distance = distance && variance ? distance : old_distance;    // latest accurate distance value
     distance = sonar.ping_cm(); // getting new current distance
   } // end of ultrasonic reading
 
@@ -312,6 +359,25 @@ void read_sensors() {         // Reading the ultrasonic- and light sensors
     for (int i = 0; i < lback_length; ++i)
       light_b[i] = digitalRead(lback[i]);
   }
+
+  vogalival = digitalRead(vogali);                                    //lese Sensoren aus...
+  volival = digitalRead(voli);
+  vomival = digitalRead(vomi);
+  voreval = digitalRead(vore);
+  vogareval = digitalRead(vogare);                                      //...und speichere sie ab
+
+/*
+  if (iteration % 50 == 0) {
+
+    digitalWrite(ustr,LOW);
+    delayMicroseconds(2);
+    digitalWrite(ustr,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(ustr,LOW);
+    usval = pulseIn(usec,HIGH);
+    distance = (usval/2)/29;
+
+  } */
 
 } // end of read_sensors
 
@@ -327,6 +393,115 @@ uint8_t read_X_pins (int* pins, uint8_t lenght) {
 }
 
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void follow_line_sub() {    // line-following logic is going to happen here !
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
+  uint8_t tmp = 0;
+
+  for (int i = 0; i < lmain_length; ++i)
+    tmp |= light_m[i]<<i;
+
+  switch(tmp & 0x07){
+    case 0x00: // 000
+      geradeaus();
+      break;
+
+    case 0x01: // 001
+      starkrechts();
+      break;
+
+    case 0x03: // 011
+      leichtrechts();
+      break;
+
+    case 0x04: // 100
+      starklinks();
+      break;
+
+    case 0x06: // 110
+      leichtlinks();
+      break;
+
+    case 0x02: // 010
+    case 0x05: // 101
+      // ruecksetzen();
+      error();
+      break;
+
+    case 0x07: // 111
+      geradeaus();
+      break;
+  }
+
+
+  switch(tmp & 0x18){ //die aeuseren
+    case 0x00: // 00
+      //speed = /*const*/;
+      geradeaus();
+      break;
+
+    case 0x08: // 01
+      starkrechts();
+      break;
+
+    case 0x10: // 10
+      starklinks();
+      break;
+
+    case 0x18: // 11
+      // ruecksetzen();
+      error();
+      // direction = DIRECTION_NON; // ?
+      break;
+  }
+
+}
+
+
+void follow_paul () {
+  if  ((vogalival == 1)&&(volival == 0)&&(vomival == 0)&&(voreval == 0)&&(vogareval == 1)){
+    geradeaus();
+  }
+  else if ((vogalival == 1)&&(volival == 1)&&(vomival == 1)&&(voreval == 1)&&(vogareval == 1)){
+    geradeaus();
+  }
+  else if ((vogalival == 1)&&(volival == 1)&&(vomival == 0)&&(voreval == 0)&&(vogareval == 1)){
+    leichtrechts();
+  }
+  else if ((vogalival == 1)&&(volival == 0)&&(vomival == 0)&&(voreval == 1)&&(vogareval == 1)){
+    leichtlinks();
+  }
+  else if (((vogalival == 1)&&(volival == 1)&&(vomival == 1)&&(voreval == 1)&&(vogareval == 0))
+          ||((vogalival == 1)&&(volival == 1)&&(vomival == 1)&&(voreval == 0)&&(vogareval == 0))
+          ||((vogalival == 1)&&(volival == 1)&&(vomival == 0)&&(voreval == 0)&&(vogareval == 0))
+          ||((vogalival == 1)&&(volival == 0)&&(vomival == 0)&&(voreval == 0)&&(vogareval == 0))){
+    starkrechts();
+    hardturnlinks = false;
+  }
+  else if (((vogalival == 0)&&(volival == 1)&&(vomival == 1)&&(voreval == 1)&&(vogareval == 1))
+          ||((vogalival == 0)&&(volival == 0)&&(vomival == 1)&&(voreval == 1)&&(vogareval == 1))
+          ||((vogalival == 0)&&(volival == 0)&&(vomival == 0)&&(voreval == 1)&&(vogareval == 1))
+          ||((vogalival == 0)&&(volival == 0)&&(vomival == 0)&&(voreval == 0)&&(vogareval == 1)))  {
+    starklinks();
+    hardturnlinks = true;
+  }
+  else if ((vogalival == 0)&&(volival == 0)&&(vomival == 0)&&(voreval == 0)&&(vogareval == 0)){
+    if (hardturnlinks == true){
+      starklinks();
+    }
+    else {
+      starkrechts();
+    }
+  }
+  else {
+    stopp();
+  }
+
+}
+
+/*
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void follow_line_sub(bool slow = false) {    // line-following logic is going to happen here !
@@ -377,15 +552,15 @@ void follow_line_sub(bool slow = false) {    // line-following logic is going to
 
   switch(tmp & 0x18){ //die aeuseren
     case 0x00: // 00
-      //speed = /*const*/;
+      //speed = // const // ;
       break;
 
     case 0x08: // 01
-      speed = SPEED_VSLOW;
+      speed = SPEED_SLOW;
       direction = DIRECTION_RIGHT_2 / (slow + 1);
 
     case 0x10: // 10
-      speed = SPEED_VSLOW;
+      speed = SPEED_SLOW;
       direction = DIRECTION_LEFT_2 / (slow + 1);
       break;
 
@@ -397,6 +572,7 @@ void follow_line_sub(bool slow = false) {    // line-following logic is going to
 
 } // end of follow_line_sub
 
+*/
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -405,9 +581,10 @@ void follow_line() {
   readLight = true;
   askForDist = true;
 
-  follow_line_sub();
+  // follow_line_sub();
+  follow_paul();
 
-  if (old_distance <= 15) { // close in on box now.
+  if (old_distance <= 24) { // close in on box now.
     readLight = true;
     readSecond = true;
     askForDist = true;
@@ -429,13 +606,12 @@ void set_motors() {           // controlling direction happens here
   if (direction == DIRECTION_NON) {   // no direction
     OnFwd(OUT_R, speed);
     OnFwd(OUT_L, speed);
-    return;   // we do not need to check the other cases, return void.
+    // return;   // we do not need to check the other cases, return void.
   }
 
   if ( abs(direction) <= speed) {     // the change in direction is relatively small
     OnFwd(OUT_R, speed - direction);
     OnFwd(OUT_L, speed + direction);
-    return;
   } else {                            // the change in direction is quite big
     int adir = abs(direction);
     direction < 0 ? OnFwd(OUT_R, (adir + speed) / 2 ) : OnRev(OUT_R, adir - speed);
@@ -468,17 +644,27 @@ void close_in() {             // closing in on the box
 
   // using line-following abstraction here.
 
-  follow_line_sub(true);
+  slow_factor = 0.5;
 
-  speed = old_distance <= 6 ? SPEED_VSLOW : SPEED_SLOW;
+  // follow_line_sub();
+  follow_paul();
+
+  if (analogRead(A6) < 128)
+    leichtrechts();
+  if (analogRead(A7) < 128)
+    leichtlinks();
+
+  // speed = old_distance <= 6 ? SPEED_VSLOW : SPEED_SLOW;
 
 
-  close_enough = (old_distance <= 3 && wasBall) || (isBall && !wasBall);
+  close_enough = (old_distance <= 4 && wasBall) || (isBall && !wasBall);
 
   if (close_enough) { // going to next stage ...
     readLight = true;
     readSecond = false;
     askForDist = true;
+
+    slow_factor = 1.0;
 
     current_action = states[++own_state];
   }
@@ -495,9 +681,14 @@ void close_in() {             // closing in on the box
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void close_hatch() {        // actually Grabbing the ball. verifying with a sensor?
   /////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  stopp();
 
-  newPos = SERVO_CLOSE;
-  current_action = &servo_moving;
+  pos = SERVO_CLOSE;
+  myServo.write(pos);
+  // current_action = &servo_moving;
+
+  current_action = states[++own_state];
 
 } // end of close_hatch
 
@@ -507,8 +698,13 @@ void close_hatch() {        // actually Grabbing the ball. verifying with a sens
 void open_hatch() {        // actually Grabbing the ball. verifying with a sensor?
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
-  newPos = SERVO_OPEN;
-  current_action = &servo_moving;
+  stopp();
+
+  pos = SERVO_OPEN;
+  myServo.write(pos);
+  // current_action = &servo_moving;
+
+  current_action = states[++own_state];
 
 } // end of open_hatch
 
@@ -519,10 +715,10 @@ void servo_moving() {     // actually moving the servo
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
   if (pos < newPos)
-    ++pos;
+    pos += 10;
 
   if (pos > newPos)
-    --pos;
+    pos -= 10;
 
   myServo.write(pos);
   delay(1);
@@ -544,6 +740,16 @@ void init_prog() { // initializing (round 2)
 
   while (digitalRead(keysensor)) delay(20);
 
+//   OnFwd(OUT_R, 150);
+//   OnFwd(OUT_L, 150);
+// 
+//   delay(500);
+//   
+//   delay(500);
+// 
+//   OFF(OUT_R);
+//   OFF(OUT_L);
+
   delay(2000);
 
   askForDist = true;
@@ -554,20 +760,50 @@ void init_prog() { // initializing (round 2)
 
 
 
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void turn() {     // Turning the robot, Software version
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-  readLight = true;
-  readSecond = true;
-  askForDist = false;
+  if (turnstate == 0) {
+    readLight = true;
+    readSecond = true;
+    askForDist = false;
+    analogWrite(OUT_L, (255-turnspeed));
+    digitalWrite(motor_left_reverse, HIGH);
+    analogWrite(OUT_R, (turnspeed));
+    digitalWrite(motor_right_reverse, LOW);
+    delay(50);
+    while (digitalRead(lmain[4]))
+      delayMicroseconds(50);
+    while (!digitalRead(lmain[4]))
+      delayMicroseconds(50);
+    analogWrite(OUT_L, (255-turnspeed - 20));
+    digitalWrite(motor_left_reverse, HIGH);
+    analogWrite(OUT_R, (turnspeed - 20));
+    digitalWrite(motor_right_reverse, LOW);
+    while (digitalRead(lmain[0]))
+      delayMicroseconds(50);
+    analogWrite(OUT_L, (255-turnspeed - 40));
+    digitalWrite(motor_left_reverse, HIGH);
+    analogWrite(OUT_R, (turnspeed - 40));
+    digitalWrite(motor_right_reverse, LOW);
+    while (digitalRead(lmain[1]))
+      delayMicroseconds(50);
+    while (digitalRead(lmain[2]))
+      delayMicroseconds(50);
+    analogWrite(OUT_L, 0);
+    digitalWrite(motor_left_reverse, LOW);
+    analogWrite(OUT_R, 0);
+    digitalWrite(motor_right_reverse, LOW);
+    turnstate = 2;
+  }
 
-  /*
+  distance = 0;
+  old_distance = 25;
+  variance = 25;
 
-  ... do stuff ...
-
-  */
-  delay(500);
-
+  delay(50);
 
   readLight = true;
   readSecond = false;
@@ -595,7 +831,7 @@ void MotorUse(int pin, int val, bool reverse) {    // actually setting the motor
   /////////////////////////////////////////////////////////////////////////////////////////////////
 
   /*
-  if (DEBUG && iteration % 5 == 0) {
+  if (DEBUG) {
     Serial.print("pin: ");
     Serial.print(pin);
     Serial.print(", gesch: ");
@@ -605,8 +841,21 @@ void MotorUse(int pin, int val, bool reverse) {    // actually setting the motor
     Serial.println("");
   }
   */
-  analogWrite(pin, val * reverse);
-  analogWrite(pin + 4, val * (not reverse));
+
+  if (reverse) {
+    analogWrite(pin, 255 - val);
+    digitalWrite(pin + 4, HIGH);
+  } else {
+    analogWrite(pin, val);
+    digitalWrite(pin + 4, LOW);
+  }
+
+//  reverse ? analogWrite(pin, val), analogWrite(pin + 4, 0)
+//          : analogWrite(pin, 0),   analogWrite(pin + 4, val) ;
+
+  // analogWrite(pin, val * reverse);
+  // analogWrite(pin + 4, val * (not reverse));
+
 } // end of MotorUse
 
 
@@ -615,9 +864,9 @@ void MotorUse(int pin, int val, bool reverse) {    // actually setting the motor
 void error() {      // an error happened - help
 ///////////////////////////////////////////////////////////////////////////////////////////////////
   digitalWrite(LED_BUILTIN,HIGH);
-  delay(50);
+  delay(20);
   digitalWrite(LED_BUILTIN,LOW);
-  delay(50);
+  delay(20);
 }
 
 
@@ -638,4 +887,63 @@ void print_mesg() {
 
 
 }
+
+void stopp() {
+  analogWrite(molipl, 0);
+  digitalWrite(molimi, LOW);
+  analogWrite(morepl, 0);
+  digitalWrite(moremi, LOW);
+}
+
+void geradeaus() {
+  analogWrite(molipl, maxspeed * slow_factor);
+  digitalWrite(molimi, LOW);
+  analogWrite(morepl, maxspeed * slow_factor);
+  digitalWrite(moremi, LOW);
+}
+
+void leichtlinks() {
+  analogWrite(molipl, (maxspeed-softturn * slow_factor));
+  digitalWrite(molimi, LOW);
+  analogWrite(morepl, maxspeed * slow_factor);
+  digitalWrite(moremi, LOW);
+}
+
+void leichtrechts() {
+  analogWrite(molipl, maxspeed * slow_factor);
+  digitalWrite(molimi, LOW);
+  analogWrite(morepl, (maxspeed-softturn * slow_factor));
+  digitalWrite(moremi, LOW);
+}
+
+void starklinks() {
+  analogWrite(molipl, (255-hardturn) * slow_factor);
+  digitalWrite(molimi, HIGH);
+  analogWrite(morepl, (hardturn*hardturnfaktor) * slow_factor);
+  digitalWrite(moremi, LOW);
+}
+
+void starkrechts() {
+  analogWrite(molipl, (hardturn*hardturnfaktor) * slow_factor);
+  digitalWrite(molimi, LOW);
+  analogWrite(morepl, (255-hardturn) * slow_factor);
+  digitalWrite(moremi, HIGH);
+}
+
+/*
+void turn(){
+  analogWrite(molipl, (255-turnspeed));
+  digitalWrite(molimi, HIGH);
+  analogWrite(morepl, (turnspeed));
+  digitalWrite(moremi, LOW);
+}
+*/
+
+void ruecksetzen(){
+  analogWrite(molipl, (255-ruecksetzspeed) * slow_factor);
+  digitalWrite(molimi, HIGH);
+  analogWrite(morepl, (255-ruecksetzspeed) * slow_factor);
+  digitalWrite(moremi, HIGH);
+}
+
 
